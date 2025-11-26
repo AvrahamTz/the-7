9b520e1d5d7fddb3a62f34929838c2e1e26a8139
Avraham <azoreff@gmail.com>
2025-11-26 15:37:02 +0200

    some classes

---
+++ rooms.py
@@ -0,0 +1,13 @@
+class Rooms:
+    def __init__(self):
+        self.rooms = 10 
+        self.per_room = 8
+        self.dorms = [] 
+    # def to_buildings(self,solider):
+    #     for i in range(1,self.rooms+1):
+    #         for j in range(1,self.per_room+1):
+
+
+
+
+
